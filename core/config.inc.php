ba52a578d8caf6ded2b08bc83c0f385e52463d23
andresllontop <llontopdiazandres@gmail.com>
2021-05-25 15:30:41 -0500

    comit final

--- core/config.inc.php
+++ core/config.inc.php
@@ -16,15 +16,15 @@ define('VISA_DEV_URL_AUTHORIZATION', 'https://apisandbox.vnforappstest.com/api.a
 define('VISA_DEV_URL_TOKENIZATION', 'https://apisandbox.vnforappstest.com/api.ecommerce/v2/ecommerce/token/card/' . VISA_DEV_MERCHANT_ID . '/');
 
 // Producción Visa
-define('VISA_PRD_MERCHANT_ID', '*********');
-define('VISA_PRD_USER', '*******************');
-define('VISA_PRD_PWD', '*******');
+define('VISA_PRD_MERCHANT_ID', '650189205');
+define('VISA_PRD_USER', 'victormejia.465@gmail.com');
+define('VISA_PRD_PWD', '$5@?T@xY');
 define('VISA_PRD_URL_SECURITY', 'https://apiprod.vnforapps.com/api.security/v1/security');
 define('VISA_PRD_URL_SESSION', 'https://apiprod.vnforapps.com/api.ecommerce/v2/ecommerce/token/session/' . VISA_PRD_MERCHANT_ID);
 define('VISA_PRD_URL_JS', 'https://static-content.vnforapps.com/elements/v1/payform.min.js');
 define('VISA_PRD_URL_CSS', 'https://static-content.vnforapps.com/elements/v1/payform.min.css');
 define('VISA_PRD_URL_AUTHORIZATION', 'https://apiprod.vnforapps.com/api.authorization/v3/authorization/ecommerce/' . VISA_PRD_MERCHANT_ID);
-define('VISA_PRD_URL_TOKENIZATION', 'https://apiprod.vnforapps.com/api.ecommerce/v2/ecommerce/token/card/' . VISA_DEV_MERCHANT_ID . '/');
+define('VISA_PRD_URL_TOKENIZATION', 'https://apiprod.vnforapps.com/api.ecommerce/v2/ecommerce/token/card/' . VISA_PRD_MERCHANT_ID . '/');
 
 // Configuración visa
 define('VISA_MERCHANT_ID', VISA_DEVELOPMENT ? VISA_DEV_MERCHANT_ID : VISA_PRD_MERCHANT_ID);
