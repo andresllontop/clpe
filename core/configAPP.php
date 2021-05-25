902ca76b81bd1a273e115d31565d711c2098a96e
andresllontop <llontopdiazandres@gmail.com>
2021-05-25 15:37:01 -0500

    edit

--- core/configAPP.php
+++ core/configAPP.php
@@ -3,17 +3,17 @@ const SERVER = "localhost";
 //const SERVER = "http://clpe5.com/";
 const DB = "clpecom_aula";
 
-//const USER = "clpecom_user";
-//const PASS = "(ClPeCoM)";
-//const SGDB = "mysql:host=localhost;dbname=clpecom_aula";
+//const USER = "*";
+//const PASS = "*";
+//const SGDB = "*";
 const USER = "root";
 const PASS = "";
-const SGDB = "mysql:host=localhost;dbname=clpecom_aula";
+const SGDB = "*";
 
-const METHOD = "AES-256-CBC";
-const SECRET_KEY = '$BP@2019';
-const SECRET_IV = '121314';
+const METHOD = "*";
+const SECRET_KEY = '*';
+const SECRET_IV = '*';
 
-const TOKEN_METHOD = "AES-256-CBC";
-const TOKEN_SECRET_KEY = '92AE31A73AEEB2A3';
-const TOKEN_SECRET_IV = '0123456789ABCDEF';
+const TOKEN_METHOD = "*";
+const TOKEN_SECRET_KEY = '*';
+const TOKEN_SECRET_IV = '*';
